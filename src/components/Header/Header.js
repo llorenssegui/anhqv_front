6e9e214a26238fa7cae9a9f82b10ff2114b7404e
Llorenç Seguí Capllonch <llorencseguicapllonch@MacBook-Pro.home>
2019-01-04 00:34:59 +0100

    Link in App Bar. Not finsih

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -6,6 +7,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Body from '../Body/Body.js'
 
 const styles = {
     root: {
@@ -30,16 +32,19 @@ class Header extends React.Component {
         const { classes } = this.props;
         return (
             <div className={classes.root}>
+                <Router>
                 <AppBar position="static">
                     <Toolbar>
                     <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h6" color="inherit" className={classes.grow}>
-                        Aqui No Hay Quien Viva
-                    </Typography>
+                    
+                        <Typography variant="h6" color="inherit" className={classes.grow}>
+                            <Link to="/">Aqui no hay Quien Viva</Link>
+                        </Typography>
                     </Toolbar>
                 </AppBar>
+                </Router>
             </div>
         );
     };
