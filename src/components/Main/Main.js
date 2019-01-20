48e93836c5d7cb3987d4c01fbfdb5cd4506216b0
Llorenç Seguí Capllonch <llorencseguicapllonch@MacBook-Pro.home>
2019-01-20 22:27:55 +0100

    Search component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Body from '../Body/Body.js';
 import CharacterClips from '../CharacterClips/CharacterClips.js';
+import Search from '../Search/Search.js';
 import ClipPlayer from '../ClipPlayer/ClipPlayer.js';
 
 class Main extends React.Component {
@@ -18,6 +19,7 @@ class Main extends React.Component {
                   <Route exact path='/personajes' component={Body} />
                   <Route exact path='/personajes/:characterId/clips' component={CharacterClips} />
                   <Route exact path='/clips/:clipId' component={ClipPlayer} />
+                  <Route exact path='/search' component={Search} />
                </Switch>
             </BrowserRouter>
         );
